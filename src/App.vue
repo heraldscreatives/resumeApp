e8891c9360c72016159fcb69472b68fc1a43db03
victorgis <essangvictor@gmail.com>
2023-02-28 22:47:29 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Main,
+    Footer,
+  },
+};
+</script>
+
+<style>
+#app {
+  /* display: grid; */
+  font-family: "Raleway", sans-serif;
+  margin: 0 auto;
+  /* -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale; */
+  /* text-align: center; */
+  /* align-content: center;
+  color: #2c3e50; */
+}
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="header-nav">
+    <nav>
+      <h1>Resume App</h1>
+      <div class="socials">
+        <ul>
+          <li>
+            <a href="https://veespatial.space"
+              ><i class="fa-solid fa-globe color-fa"></i
+            ></a>
+          </li>
+          <li>
+            <a href="https://twitter.com/victorgis"
+              ><i class="fa-brands fa-twitter color-fa"></i
+            ></a>
+          </li>
+          <li>
+            <a href="thhps://linkedin.com/in/victoressang"
+              ><i class="fa-brands fa-linkedin color-fa"></i
+            ></a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ResumeAppHeader",
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style scoped>
+.header-nav {
+  background: #3564;
+  padding: 8px;
+}
+nav {
+  display: flex;
+  justify-content: space-between; /* or center, flex-start, flex-end */
+  align-items: center; /* or flex-start, flex-end, baseline */
+  padding: 0 20px;
+}
+
+.socials ul {
+  display: inline-flex;
+}
+.socials li {
+  list-style-type: none;
+  padding: 0 20px;
+}
+
+.color-fa {
+  color: black;
+}
+</style>
